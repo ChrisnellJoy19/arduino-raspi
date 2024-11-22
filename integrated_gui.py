17f805911be43d1fe13457d959744af449831fbc
ChrisnellJoy19 <cjlimpiada03@gmail.com>
2024-11-22 17:49:29 +0800

    updated

--- integrated_gui.py
+++ integrated_gui.py
@@ -236,5 +236,5 @@ class Root(tk.Tk):
 
 
 if __name__ == '__main__':
-    machine = Machine(port=None, gsm_port=None, debug=True)
+    machine = Machine(port="/dev/ttyACM0", gsm_port=None, debug=True)
     root = Root(machine=machine)
